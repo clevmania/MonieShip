78c525993f8bca4b26bc897ab69057908ecd78e7
clevmania <ernestcleverest@yahoo.com>
2025-08-08 09:48:21 +0100

    added app colors

--- app/src/main/java/com/example/monieship/ui/theme/Color.kt
+++ app/src/main/java/com/example/monieship/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.monieship.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val AppPurple = Color(0xFF5A3FE3)
+val AppPurpleLight = Color(0xFFF0EDFF)
+val AppOrange = Color(0xFFFE9901)
+val TextGray = Color(0xFF9E9E9E)
+val LightGray = Color(0xFFF5F5F5)
+val DarkGray = Color(0xFF333333)
+val LightGreen = Color(0xFFE8F8E8)
+val DarkGreen = Color(0xFF35B369)
+val LightOrange = Color(0xFFFFF6E8)
